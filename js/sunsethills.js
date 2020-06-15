08eed1be81f9f8c1bd0903e3dd26bf5cb4b9f69e
mwmcguire <mike@mmcguire.io>
2020-06-15 15:19:48 -0400

    initial commit and layout done

---
+++ js/sunsethills.js
@@ -0,0 +1,7 @@
+// An array of buildings is facing the sun
+// The heights of each building from west to east is given in an integer array
+// Which buildings will be able to see the sunset given the array of heights
+
+// East -- [3, 2, 4, 7, 6, 9] -- West
+// Input: Allow the user to enter their own values into an array before determining who can see the sunset.
+// Output: 4 + [3, 4, 7, 9]  Show output in a creative way.  Must show the number of buildings AND which buildings in the array
